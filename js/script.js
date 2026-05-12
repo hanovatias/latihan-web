79dd9290f044896c00026f17acda2c0c8e993b73
hanovatias <hanovatias@gmail.com>
2026-05-12 09:40:01 +0700

    refactor : pisah css dan js ke file terpisah
    1.pindahin kode css ke folder css/style.css
    2.pindahin kode js ke folder js/script.js
    3.rapihin index.html biar mudah di maintain

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let jumlahKlik = 0;
+        function pencetTombol() {
+            jumlahKlik = jumlahKlik + 1;
+            document.getElementById('hasil').innerHTML = 
+            'ANJIR BISA BRO 🔥 Udah diklik ' + jumlahKlik + 'x';
+        }
